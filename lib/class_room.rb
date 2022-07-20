fe0570bae3016f1d191bf38f8aae4905df3842b3
askumardev <askumar.dev@outlook.com>
2022-07-20 19:19:25 +0530

    rspec double implementation

--- lib/class_room.rb
+++ lib/class_room.rb
@@ -1,3 +1,4 @@
+
 class ClassRoom 
   def initialize(students) 
     @students = students 
--- spec/class_room_spec.rb
+++ spec/class_room_spec.rb
@@ -1,3 +1,6 @@
+# RSpec Doubles, also known as RSpec Mocks. A Double is an object which can “stand in” for 
+# another object. You’re probably wondering what that means exactly and why you’d need one.
+
 require 'class_room'
 describe ClassRoom do 
   it 'the list_student_names method should work correctly' do 
